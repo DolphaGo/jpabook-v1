bef32e9f076a1766ea8a4de5afca7e9af05a7ed5
DolphaGo <adamdoha@naver.com>
2020-12-01 13:53:06 +0900

    init : create Project

---
+++ dolphago/src/test/java/com/jpabook/dolphago/DolphagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpabook.dolphago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolphagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
