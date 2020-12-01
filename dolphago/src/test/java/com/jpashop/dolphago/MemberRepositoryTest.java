5f4bbecaf41d8005a09bafef0c400ff328aeeea3
DolphaGo <adamdoha@naver.com>
2020-12-02 02:57:19 +0900

    delete : unusing Testcode

--- dolphago/src/test/java/com/jpashop/dolphago/MemberRepositoryTest.java
+++ dolphago/src/test/java/com/jpashop/dolphago/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.jpashop.dolphago;
 
 
+import com.jpashop.dolphago.domain.Member;
+import com.jpashop.dolphago.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class MemberRepositoryTest {
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional
     @Rollback(value = false)
     public void testMember() throws Exception{
         //given
-        Member member=new Member();
-        member.setUsername("memberA");
-
-        //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); // True
-        System.out.println("findMember == member :"+ (findMember == member));
 
     }
 }
