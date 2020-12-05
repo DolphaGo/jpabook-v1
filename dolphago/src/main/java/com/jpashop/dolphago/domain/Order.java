d21b4b6ca13f347d91e6e2d7e711031bbec5f858
DolphaGo <adamdoha@naver.com>
2020-12-06 03:02:56 +0900

    refactor : PK staratege to IDENTITY

--- dolphago/src/main/java/com/jpashop/dolphago/domain/Order.java
+++ dolphago/src/main/java/com/jpashop/dolphago/domain/Order.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Table(name = "orders")
 public class Order {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private Long id;
 
